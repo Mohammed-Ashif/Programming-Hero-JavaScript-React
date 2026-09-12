8a18962e179979767c9f0c84eee0a59974ca3845
Mohammed Ashif <asifiqubal3579@gmail.com>
2026-09-12 13:18:12 +0600

    Initial Commit!

--- BMI.js
+++ BMI.js
@@ -17,32 +17,50 @@ function calculate_BMI (weight, height) {
 }
 
 // "rl" is simply a variable name, but "rl" is a common abbreviation for readline.
-rl.question("Enter your weight (kg): ", weightInput => {    // weightInput and heightInput is initially a string. 
-  rl.question("Enter your height (m): ", heightInput => {   // That's why later you use parseFloat().
+rl.question("Enter your weight (kilograms): ", weightInput => {    // weightInput and heightInput is initially a string. 
+  rl.question("Enter your height (meters): ", heightInput => {   // That's why later you use parseFloat().
     const weight = parseFloat(weightInput); // Convert weight to a number
     const height = parseFloat(heightInput); // Convert height to a number
 
     // Validating the input
     if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) { 
       console.log("Please enter valid positive numbers."); 
-    } else {
+    } 
+    
+    else {
+
+      // Adult BMI limits are defined by the CDC as: 
+      // underweight (below 18.5), 
+      // healthy weight (18.5–24.9), 
+      // overweight (25–29.9), 
+      // and obesity (30+), with obesity further divided into 
+      //                    Class I (30–34.9), 
+      //                    Class II (35–39.9), 
+      //                    and Class III (40+).
+
       const bmi = calculate_BMI(weight, height);
       console.log("BMI: ", bmi.toFixed(2));
+
+      if (bmi > 0 && bmi < 18.5) {
+        console.log ("Underweight");
+      } else if (bmi >= 18.5 && bmi < 25) {
+        console.log ("Healthy Weight");
+      } else if (bmi >= 25 && bmi < 30) {   
+        console.log ("overweight");
+      } else if (bmi >= 30 && bmi < 35) {   
+        console.log ("Obesity Class I");
+      } else if (bmi >= 35 && bmi < 40) {   
+        console.log ("Obesity Class II");
+      }  else if (bmi >= 40) {   
+        console.log ("Obesity Class III");
+      } else {
+        console.log ("Invalid");
+      } 
     }
+
+    
 
     // Without closing the readline interface, Node.js may continue waiting for input.
     rl.close();
   });
 });
-
-// TODO list:
-// Adult BMI limits are defined by the CDC as: underweight (below 18.5), 
-// healthy weight (18.5–24.9), overweight (25–29.9), 
-// and obesity (30+), with obesity further divided into 
-//                    Class I (30–34.9), 
-//                    Class II (35–39.9), 
-//                    and Class III (40+).
-
-if (bmi ) {
-  console.log ("");
-}
